9c21a4a2483858c80ce324c881243c4324e4e79b
SUKHAD SHARMA <mykeyman777@gmail.com>
2026-03-17 10:01:34 +0530

    Pushing Root

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from app.routers import simulation
 from dotenv import load_dotenv
 from app.database.db import engine
 from app.database.models import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -15,3 +16,16 @@ app.include_router(simulation.router)
 @app.get("/")
 def root():
     return {"message": "StratSim AI backend is running"}
+
+origins = [
+    "http://localhost:5173",  # your Vite frontend URL
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
